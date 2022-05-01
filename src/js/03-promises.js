1b8392a16b78fd16d9e2ce073d2cd9c23c312132
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-05-01 12:36:48 +0300

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,44 @@
 import Notiflix from 'notiflix';
-
 const refs = {
-  delayInput: document.querySelector('[name=delay]'),
-  stepInput: document.querySelector('[name=step]'),
-  amountInput: document.querySelector('[name=amount]'),
-  submitBtn: document.querySelector('button'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  form: document.querySelector('.form').addEventListener('submit', onSubmit),
+};
+
+function onSubmit(event) {
+  event.preventDefault();
+  const enteredValues = {
+    delay: parseInt(refs.delay.value),
+    step: parseInt(refs.step.value),
+    amount: parseInt(refs.amount.value),
+  };
+  createPromiseConditions(enteredValues);
 }
 
-refs.submitBtn.addEventListener('submit', onSubmitBtn);
-
-
-function onSubmitBtn(event) {
-    event.preventDefault();
-  const data = {
-    delay: Number(refs.delayInput.value),
-    step: Number(refs.stepInput.value),
-    amount: Number(refs.amountInput.value),
-  }
-  
-  newPromiseCreation(data);
-}
-  
-function  newPromiseCreation({ delay, step, amount }) {
-  let calculatedDelay = delay;
+function createPromiseConditions({ delay, step, amount }) {
+  let totalDelay = delay;
   for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, calculatedDelay)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-     calculatedDelay += step;
+    createPromise(i, totalDelay)
+      .then(({ position, delay }) => {
+       Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    totalDelay += step;
   }
+  let sumOfAllDaleys = 0;
+  sumOfAllDaleys += totalDelay;
+
+  setTimeout(() => {
+    Notiflix.Notify.info(`All ${amount} promises were generated in ${sumOfAllDaleys}ms`);
+  }, sumOfAllDaleys);
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-    
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
